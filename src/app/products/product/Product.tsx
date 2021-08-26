c6f578082f923e46d0bce5c4abc47da67ae23d6e
Baartooo <bartek.gajos19@gmail.com>
2021-08-26 21:17:08 +0200

    product description style

--- src/app/products/product/Product.tsx
+++ src/app/products/product/Product.tsx
@@ -5,14 +5,18 @@ import { ProductItem } from './Product.types';
 import s from './Product.module.scss';
 
 interface ProductProps {
-  item: ProductItem
+  item: ProductItem;
 }
 
 export const Product = ({ item }: ProductProps) => {
   return (
     <div className={s.product}>
       <div className={s.product__imageWrapper}>
-        <img src={item.image} alt={item.name} className={s.product__image}/>
+        <img src={item.image} alt={item.name} className={s.product__image} />
+      </div>
+      <div className={s.product__details}>
+        <h2 className={s.product__name}>{item.name}</h2>
+        <p className={s.product__description}>{item.description}</p>
       </div>
     </div>
   );
