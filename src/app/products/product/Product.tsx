a9cb8c3d2ada8777cefa993b06e9a644875aef9f
Baartooo <bartek.gajos19@gmail.com>
2021-08-28 11:46:39 +0200

    overlay in progress; promo label refactor

--- src/app/products/product/Product.tsx
+++ src/app/products/product/Product.tsx
@@ -4,6 +4,7 @@ import { ProductItem } from './Product.types';
 
 import { Rating } from 'app/shared/rating/Rating';
 import { Button } from 'app/shared/button/Button';
+import { PromoLabel } from 'app/shared/promo-label/PromoLabel';
 
 import s from './Product.module.scss';
 
@@ -15,8 +16,10 @@ export const Product = ({ item }: ProductProps) => {
 
   return (
     <div className={s.product}>
+      {item.promo && <PromoLabel />}
       <div className={s.product__imageWrapper}>
         <img src={item.image} alt={item.name} className={s.product__image} />
+        {!item.active && <div className={s.product__inactiveOverlay} />}
       </div>
       <div className={s.product__details}>
         <h2 className={s.product__name}>{item.name}</h2>
---
+++ src/app/shared/promo-label/PromoLabel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './PromoLabel.module.scss';
+
+export const PromoLabel = () => (
+  <div className={s.promoLabel}>
+    <span className={s.promoLabel__content}>Promo</span>
+  </div>
+);
