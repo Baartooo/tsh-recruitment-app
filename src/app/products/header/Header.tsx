32a0c583fde06eef7e4caa4f97e4be83bccbc7b3
Baartooo <bartek.gajos19@gmail.com>
2021-08-29 21:11:15 +0200

    search icon

--- src/app/products/header/Header.tsx
+++ src/app/products/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import s from './Header.module.scss';
+import { Search } from '../../shared/search/Search';
 
 
 export const Header = () => {
@@ -9,6 +10,13 @@ export const Header = () => {
       <div className={s.header__wrapper}>
         <div className={s.header__logo}>
           <a href={'#'} className={s.header__link}>join.tsh.io</a>
+        </div>
+
+        <div className={s.header__search}>
+          <input type={'text'} className={s.header__input} placeholder={'Search'} />
+          <div className={s.header__searchIcon}>
+            <Search />
+          </div>
         </div>
       </div>
     </div>
