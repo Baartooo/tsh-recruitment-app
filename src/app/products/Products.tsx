e2369d6205c8aa826b838bd3f43ee5c0be085b22
Baartooo <bartek.gajos19@gmail.com>
2021-08-29 14:46:54 +0200

    pagination in progress

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import axios from 'axios';
 import useSWR from 'swr';
@@ -20,12 +20,27 @@ const fetcher = async (endpoint: string) => {
 export const Products = () => {
   const { data } = useSWR('/products', fetcher);
   const [items, setItems] = useState<ProductItem[]>([]);
+  const [currentPage, setCurrentPage] = useState<number | null>(null);
+
+  const ITEMS_PER_PAGE = 8;
+
+  const pages = useMemo(() => Math.ceil(items.length / ITEMS_PER_PAGE), [items.length]);
 
   useEffect(() => {
     if (data && data.items) {
-      setItems(data.items);
+      setItems(items);
+      setCurrentPage(1);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (currentPage !== null) {
+      const start = (currentPage - 1) * ITEMS_PER_PAGE;
+      const end = currentPage * ITEMS_PER_PAGE;
+      const items = data.items.slice(start, end);
+      setItems(items);
+    }
+  }, [currentPage]);
 
   return (
     <div className={s.products}>
@@ -34,9 +49,16 @@ export const Products = () => {
           ? <>
             <div className={s.products__wrapper}>
               {items.map(item => <Product item={item} key={item.id} />)}
-
             </div>
-            <Pagination itemsLength={items.length} currentPage={1} />
+            {
+              currentPage !== null &&
+              <Pagination
+                itemsLength={items.length}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesAmount={pages}
+              />
+            }
           </>
           : <ProductsEmpty />
       }
--- src/app/products/pagination/Pagination.tsx
+++ src/app/products/pagination/Pagination.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
 import s from './Pagination.module.scss';
 
 interface IPagination {
   itemsLength: number;
   currentPage: number;
+  pagesAmount: number;
+  isOnFirstPage: boolean;
+  isOnLastPage: boolean;
+  setCurrentPage: Dispatch<SetStateAction<number | null>>;
 }
 
-export const Pagination = (props: IPagination) => {
+export const Pagination = ({ itemsLength, currentPage, pagesAmount, isOnFirstPage, isOnLastPage }: IPagination) => {
+
   return (
     <div className={s.pagination}>
       <div className={s.pagination__content}>
-        <span className={s.pagination__extreme}>First</span>
-        <span className={s.pagination__extreme}>Last</span>
+        <span className={`${s.pagination__extreme} ${isOnFirstPage ? s.disabled : ''}`}>First</span>
+
+
+        <span className={`${s.pagination__extreme} ${isOnLastPage ? s.disabled : ''}`}>Last</span>
       </div>
     </div>
   );
